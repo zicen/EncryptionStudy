df19fc6823ffa81c9ccc28a1f5cc0ba33099c801
zicen <1140377034@qq.com>
2017-09-15 17:25:15 +0800

    完

--- src/com/zhenquan/encryption/数字签名/数字签名.kt
+++ src/com/zhenquan/encryption/数字签名/数字签名.kt
@@ -1,17 +1,59 @@
 package com.zhenquan.encryption.数字签名
 
+import com.zhenquan.encryption.非对称加密.RSA
 import java.security.PrivateKey
+import java.security.PublicKey
 import java.security.Signature
 import java.util.*
 
 fun main(args: Array<String>) {
+   val input =  "name=iphont&price=78888"
+    val keyPair = RSA.getKeyPair()
+    val sign = Sign.sign(input,keyPair.private!!)
+    println("sign:" + sign)
+    val verify = Sign.verify(input, keyPair.public!!, sign)
+    println("verify:"+verify)
 }
 
-object Sign{
-    fun sign(input:String,key:PrivateKey):String {
+/**
+ * 数字签名：是对非对称加密和消息摘要的组合应用 SHA256withRSA
+ * 应用场景：
+ * 1、校验用户身份（使用私钥签名，公钥校验，只要用公钥能校验通过，则该信息一定是私钥持有者发布的）
+ * 2、校验数据完整性（用解密后的消息摘要跟原文的消息摘要进行比对）
+ *
+ *
+ * 数字签名流程
+ * 1、发件人对原文进行“数字摘要加密”->RSA加密  然后将明文+密文一起发送出去（为了接收方的校验）   这个步骤就是签名的操作
+ * 2、收件人对密文进行“RSA解密”->  将解密后的内容得到其HASH值与明文的HASH值进行比对，如果不同则真实性有问题
+ *
+ *
+ *
+ *
+ *
+ */
+object Sign {
+    fun sign(input: String, key: PrivateKey): String {
+        //获取数据签名实例
         val signature = Signature.getInstance("SHA256withRSA")
         signature.initSign(key)
+        //设置数据源
+        signature.update(input.toByteArray())
         val sign = signature.sign()
         return String(Base64.getEncoder().encode(sign))
+    }
+
+    /**
+     * 校验
+     */
+    fun verify(input: String, publicKey: PublicKey, sign: String): Boolean {
+        val signature = Signature.getInstance("SHA256withRSA")
+        //初始化签名
+        signature.initVerify(publicKey)
+        //传入数据源
+        signature.update(input.toByteArray())
+        //校验签名信息
+
+        return signature.verify(Base64.getDecoder().decode(sign))
+
     }
 }
--- src/com/zhenquan/encryption/非对称加密/RSA.kt
+++ src/com/zhenquan/encryption/非对称加密/RSA.kt
@@ -1,40 +1,62 @@
 package com.zhenquan.encryption.非对称加密
 
-import java.io.ByteArrayOutputStream
-import java.security.KeyPair
-import java.security.KeyPairGenerator
-import java.security.PrivateKey
-import java.security.PublicKey
+import java.io.*
+import java.security.*
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import java.util.*
 import javax.crypto.Cipher
 
 fun main(args: Array<String>) {
     val command = "你好"
+    //程序第一次启动的时候生成秘钥对，然后保存起来使用
     val pairGenerator = KeyPairGenerator.getInstance("RSA")
     val keyPair = pairGenerator.genKeyPair()
     val publicKey = keyPair.public
     val privateKey = keyPair.private
 
+//秘钥没必要每次都生成一个新的，所以可以保存密钥对
+    val objectOutputStream = ObjectOutputStream(FileOutputStream("privateKey"))
+    objectOutputStream.writeObject(privateKey)
+    objectOutputStream.close()
 
-    val encryptByPrivateKey = RSA.encryptByPrivateKey(command, privateKey)
+    val objectOutputStream2 = ObjectOutputStream(FileOutputStream("publicKey"))
+    objectOutputStream2.writeObject(publicKey)
+    objectOutputStream2.close()
+
+    //读取密钥对
+    val objectInputStream = ObjectInputStream(FileInputStream("privateKey"))
+    val readObject = objectInputStream.readObject()
+    readObject as PrivateKey
+    objectInputStream.close()
+
+    val objectInputStream2 = ObjectInputStream(FileInputStream("publicKey"))
+    val readObject2 = objectInputStream2.readObject()
+    readObject2 as PublicKey
+    objectInputStream2.close()
+
+
+
+    //测试
+    val encryptByPrivateKey = RSA.encryptByPrivateKey(command, readObject)
     println("私钥加密：" + encryptByPrivateKey)
-    val encryptByPublicKey = RSA.encryptByPublicKey(command, publicKey)
+    val encryptByPublicKey = RSA.encryptByPublicKey(command, readObject2)
     println("公钥加密：" + encryptByPublicKey)
 
     /**
      * 分段加密  每次加密不能超过117个字节
      */
     val command2 = "你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好"
-    val encryptByPrivateKey2 = RSA.encryptByPrivateKey(command2, privateKey)
+    val encryptByPrivateKey2 = RSA.encryptByPrivateKey(command2, readObject)
     println("私钥分段加密：" + encryptByPrivateKey2)
 
-    val deEncryptByPrivateKey = RSA.deEncryptByPublicKey(encryptByPrivateKey2, publicKey)
+    val deEncryptByPrivateKey = RSA.deEncryptByPublicKey(encryptByPrivateKey2, readObject2)
     println("公钥分段解密：" + deEncryptByPrivateKey)
 
 
-    val encryptByPublicKey1 = RSA.encryptByPublicKey(command2, publicKey)
+    val encryptByPublicKey1 = RSA.encryptByPublicKey(command2, readObject2)
     println("公钥分段加密：" + encryptByPublicKey1)
-    val deEncryptByPrivateKey1 = RSA.deEncryptByPrivateKey(encryptByPublicKey1, privateKey)
+    val deEncryptByPrivateKey1 = RSA.deEncryptByPrivateKey(encryptByPublicKey1, readObject)
     println("私钥分段解密：" + deEncryptByPrivateKey1)
 }
 
@@ -43,7 +65,7 @@ object RSA {
     /**
      * 生成公钥和私钥
      */
-    fun getKeyPair(): KeyPair? {
+    fun getKeyPair(): KeyPair{
         val pairGenerator = KeyPairGenerator.getInstance("RSA")
         val keyPair = pairGenerator.genKeyPair()
         val publicKey = keyPair.public
